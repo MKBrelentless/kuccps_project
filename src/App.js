a082478b3c90d72cb752e990bef5ace353c520a7
MKBrelentless <austinmukabwa02@gmail.com>
2025-03-19 19:36:34 +0300

    Commit with updated App.js and new inputPage.html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HomePage, RecommendationPage } from "./pages";
+import HomePage from "./pages/HomePage";
+import RecommendationPage from "./pages/RecommendationPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/courses" element={<CourseList />} /> */}
         <Route path="/recommendations" element={<RecommendationPage />} />
       </Routes>
     </Router>
